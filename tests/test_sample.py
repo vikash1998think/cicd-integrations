af8fbe3ff9cf178a1e05fab900d506588af6938e
vikash1998think <88701942+vikash1998think@users.noreply.github.com>
2022-08-25 23:28:00 +0530

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,4 +4,4 @@ from apps.app import create_app
 def test_answer():
     client = create_app().test_client()
     response = client.get('/')
-    assert response.status_code == 400
+    assert response.status_code == 404
